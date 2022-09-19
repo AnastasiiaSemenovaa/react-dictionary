77e9c4e757642bb73821060506f917d2e88974a9
AnastasiiaSemenovaa <semenovaanastasiiaa@gmail.com>
2022-09-19 20:38:07 +0300

    created a form

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Dictionary() {
-  return "Hello";
+  const [word, setWord] = useState(null);
+  function Search(event) {
+    event.preventDefault();
+    alert(`Searching ${word}`);
+  }
+  function findWord(event) {
+    setWord(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <h1>What word are you looking for?</h1>
+      <form onSubmit={Search}>
+        <input
+          type="Search"
+          autofocus="on"
+          placeholder="Type it here please"
+          onChange={findWord}
+        ></input>
+        <input type="Submit" value="Search"></input>
+      </form>
+    </div>
+  );
 }
