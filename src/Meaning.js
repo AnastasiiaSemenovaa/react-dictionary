f8e46b8cfdbe24b74493543822101a193ec98bcf
AnastasiiaSemenovaa <semenovaanastasiiaa@gmail.com>
2022-09-20 20:51:15 +0300

    Update Meaning.js

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,9 +1,10 @@
 import React from "react";
 
 export default function Meaning(props) {
+  console.log(props.meaning);
   return (
     <div className="Meaning">
-      <h3>{props.meaning.partofSpeech}</h3>
+      <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
