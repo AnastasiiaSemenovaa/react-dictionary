f0703891b38a7f5a3f74ee770b379eb51a0d556d
AnastasiiaSemenovaa <semenovaanastasiiaa@gmail.com>
2022-09-25 16:10:02 +0300

    added phonetics

---
+++ src/Phonetic.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Phonetic(props) {
+  return (
+    <div>
+      <a href={props.phonetic.audio} target="_blank">
+        Listen
+      </a>
+      <br />
+      {props.phonetic.text}
+    </div>
+  );
+}
